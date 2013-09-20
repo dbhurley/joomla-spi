214b18a50a37dd13f824c828724c25bd25151b1a
David Hurley <david@websparkinc.com>
2013-09-20 02:24:31 -0500

    Updated to include DB.

--- start.php
+++ start.php
@@ -14,10 +14,12 @@ class JoomlaSingleStart
 		if(isset($_REQUEST['task']) && $_REQUEST['task']=='go')
 		{
 			self::download();
+		} elseif(isset($_REQUEST['task']) && $_REQUEST['task']=='finalise')
+		{
+			self::install();
 		} else
 		{
 			self::start();
-			file_put_contents( dirname( __FILE__).'/progress', null );
 		}
 	}
 
@@ -69,9 +71,129 @@ class JoomlaSingleStart
 		$result = curl_exec($ch);
 		curl_close($ch);
 		fclose($fp);
-		unlink( dirname( __FILE__).'/progress' );
-		unlink( dirname( __FILE__).'/start.php' );
+		
 		return $result;
+	}
+
+	function install()
+	{
+		$config = $_POST['config'];
+		$path = pathinfo(realpath(__FILE__), PATHINFO_DIRNAME);
+
+		//Random database prefix
+		$str = "";
+		$chars = "abcdefghijklmnopqrstuvwxyz";
+		$size = strlen( $chars );
+		for( $i = 0; $i < 5; $i++ ) {
+			$str .= $chars[ rand( 0, $size - 1 ) ];
+		}
+
+		$dbprefix = $str . '_';
+		$random = 'ASDFASDFASDvasd3';
+
+		$config_data = "<?php
+	class JConfig {
+		public \$offline = '0';
+		public \$offline_message = 'This site is down for maintenance.<br /> Please check back again soon.';
+		public \$display_offline_message = '1';
+		public \$offline_image = '';
+		public \$sitename = '" . $config['sitename'] . "';
+		public \$editor = 'tinymce';
+		public \$captcha = '0';
+		public \$list_limit = '20';
+		public \$access = '1';
+		public \$debug = '1';
+		public \$debug_lang = '0';
+		public \$dbtype = 'mysqli';
+		public \$host = '" . $config['hostname'] . "';
+		public \$user = '" . $config['dbusername'] . "';
+		public \$password = '" . $config['dbpassword'] . "';
+		public \$db = '" . $config['dbname'] . "';
+		public \$dbprefix = '" . $dbprefix . "';
+		public \$live_site = '';
+		public \$secret = '" . $random . "';
+		public \$gzip = '0';
+		public \$error_reporting = 'maximum';
+		public \$helpurl = 'http://help.joomla.org/proxy/index.php?option=com_help&keyref=Help{major}{minor}:{keyref}';
+		public \$ftp_host = '127.0.0.1';
+		public \$ftp_port = '21';
+		public \$ftp_user = '';
+		public \$ftp_pass = '';
+		public \$ftp_root = '';
+		public \$ftp_enable = '0';
+		public \$offset = 'UTC';
+		public \$mailer = 'mail';
+		public \$mailfrom = '';
+		public \$fromname = '';
+		public \$sendmail = '/usr/sbin/sendmail';
+		public \$smtpauth = '0';
+		public \$smtpuser = '';
+		public \$smtppass = '';
+		public \$smtphost = 'localhost';
+		public \$smtpsecure = 'none';
+		public \$smtpport = '25';
+		public \$caching = '0';
+		public \$cache_handler = 'file';
+		public \$cachetime = '15';
+		public \$MetaDesc = '';
+		public \$MetaKeys = '';
+		public \$MetaTitle = '1';
+		public \$MetaAuthor = '1';
+		public \$MetaVersion = '0';
+		public \$robots = '';
+		public \$sef = '1';
+		public \$sef_rewrite = '0';
+		public \$sef_suffix = '0';
+		public \$unicodeslugs = '0';
+		public \$feed_limit = '10';
+		public \$log_path = '" . $path . "/logs';
+		public \$tmp_path = '" . $path . "/tmp';
+		public \$lifetime = '15';
+		public \$session_handler = 'database';
+		public \$MetaRights = '';
+		public \$sitename_pagetitles = '0';
+		public \$force_ssl = '0';
+		public \$feed_email = 'author';
+		public \$cookie_domain = '';
+		public \$cookie_path = '';
+	}";
+		$config_file = $path . '/configuration.php';
+
+		file_put_contents($config_file, $config_data);
+
+		$joomla_sql = file_get_contents($path . '/installation/sql/mysql/joomla.sql');
+		$sample_sql = file_get_contents($path . '/installation/sql/mysql/sample_brochure.sql');
+
+		$joomla_sql = str_replace('#__', $dbprefix, $joomla_sql);
+		$sample_sql = str_replace('#__', $dbprefix, $sample_sql);
+
+		file_put_contents($path . '/installation/sql/mysql/joomla.sql',$joomla_sql);
+		file_put_contents($path . '/installation/sql/mysql/sample_brochure.sql',$sample_sql);
+
+			$cmd = 'mysql'
+		        . ' --host=' . $config['hostname']
+		        . ' --user=' . $config['dbusername']
+		        . ' --password=' . $config['dbpassword']
+		        . ' --database=' . $config['dbname']
+			;
+
+			echo shell_exec($cmd . ' --execute="SOURCE ' . $path .'/installation/sql/mysql/joomla.sql" 2>&1');
+			
+			$connection = mysql_connect($config['hostname'], $config['dbusername'], $config['dbpassword']);
+			mysql_select_db($config['dbname']);
+			mysql_query("INSERT INTO " . $dbprefix . "users (id, name, username, email, password, block, sendEmail, activation) VALUES (296, 'Super User', '" . $config['user'] . "', '" . $config['email']. "', '" . md5($config['pass']) . "', 0, 1, 0)");
+			mysql_query("INSERT INTO " . $dbprefix . "user_usergroup_map (user_id, group_id) VALUES (296, 8);");
+
+			if($config['data']) 
+			{
+				echo shell_exec($cmd . ' --execute="SOURCE ' . $path . '/installation/sql/mysql/sample_brochure.sql" 2>&1');
+			}
+			
+			echo shell_exec('rm -rf '.$path . '/installation');
+			unlink( dirname( __FILE__).'/progress' );
+			unlink( dirname( __FILE__).'/start.php' );
+			
+			echo json_encode(array('success'=>true));
 	}
 
 	// View
@@ -128,34 +250,82 @@ class JoomlaSingleStart
 						<?php
 						if (!is_writable(dirname($this->localfile))) {
 							$dir_class = "alert alert-danger";
-							$msg = "<p class='alert alert-danger'><span class='glyphicon glyphicon-warning-sign'></span> This path is not writable. Please change permissions before continuing.</p>";
+							$msg = "<span class='glyphicon glyphicon-warning-sign'></span> The following path is not writable. Please change permissions before continuing";
 							$continue = "disabled";
 						} else
 						{
+							file_put_contents( dirname( __FILE__).'/progress', null );
 							$dir_class = "well well-small";
-							$msg = "";
+							$msg = "<span class='glyphicon glyphicon-ok'></span> The following directory will be used";
 							$continue = "";
 						}
 						?>
 
-						<?php echo $msg; ?>
-
 				      <!-- Main component for a primary marketing message or call to action -->
-				      <div class="jumbotron">
+				      <div class="">
 				        <h1>Start Installation</h1>
 				        <p>This installer will download the latest Joomla! release and prepare the installation directly on your server. The following directory will be used:</p>
-				        <p class="<?php echo $dir_class; ?>"><?php echo dirname(__FILE__); ?></p>
-					    <p>
-					      <div class="hide progressMsg">
-						      Progress: <span class="label label-info">downloading Joomla!</span>
-					      </div>
-					      <div class="progress progress-striped ">
-					        <div class="progress-bar progress-bar-info" role="progressbar" style="width: 0%">
-					        </div>
-					      </div>
-					    </p>
-				       	<p><a id="go" class="btn btn-primary btn-lg <?php echo $continue; ?>" data-loading-text="Downloading...">Download & Start Install</a></p>
+				        <p class="<?php echo $dir_class; ?>">
+				        	<?php echo $msg; ?>: 
+				        	<?php echo "<strong>" . dirname(__FILE__) . "</strong>"; ?>
+				       	</p>
+					  </div>
+
+					<h3>Config Details</h3>
+					<div class="row">
+					<form id="config_form" role="form">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label for="config[sitename]">Site Name</label>
+								<input type="text" class="form-control" name="config[sitename]" value="" placeholder="Enter the site name" />
+							</div>
+							<div class="form-group">
+								<label for="config[user]">Admin User</label>
+								<input type="text" class="form-control" name="config[user]" value="" placeholder="Enter a username"/>
+							</div>
+							<div class="form-group">
+								<label for="config[pass]">Admin Pass</label>
+								<input type="text" class="form-control" name="config[pass]" value="" placeholder="Enter a password" />
+							</div>
+							<div class="form-group">
+								<label for="config[pass]">Admin Email</label>
+								<input type="text" class="form-control" name="config[email]" value="" placeholder="Enter an email address" />
+							</div>
+							<div class="checkbox">
+						    	<label>
+						      		<input type="checkbox" name="config[data]"> Install sample data
+						    	</label>
+						  	</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<label for="database[host]">Database Host</label>
+								<input type="text" class="form-control" name="config[hostname]" value="" placeholder="localhost"/>
+							</div>
+							<div class="form-group">
+								<label for="database[name]">Database Name</label>
+								<input type="text" class="form-control" name="config[dbname]" value="" placeholder="Enter database name" />
+							</div>
+							<div class="form-group">
+								<label for="database[username]">Database Username</label>
+								<input type="text" class="form-control" name="config[dbusername]" value="" placeholder="Enter database username" />
+							</div>
+							<div class="form-group">
+								<label for="database[password]">Database Password</label>
+								<input type="text" class="form-control" name="config[dbpassword]" value="" placeholder="Enter database password" />
+							</div>
+						</div>
+					</form>
+					</div>
+					<hr />
+				      <div class="hide progressMsg">
+					      <h3>Progress: <span class="label label-info pull-right">Downloading Joomla!</span></h3>
+				      <div class="progress progress-striped ">
+				        <div class="progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+				        </div>
 				      </div>
+				      </div>
+			       	<p><a id="go" class="btn btn-primary btn-lg <?php echo $continue; ?>" data-loading-text="Downloading...">Download & Start Install</a></p>
 
 				    </div> <!-- /container -->
 
@@ -174,19 +344,48 @@ class JoomlaSingleStart
 							 setTimeout( function() { progress(); }, 100 );
 						 }
 					 }
+
+					 function install()
+					 {
+					 	//generate data string for ajax call
+						var dataString = '';
+						var $form = jQuery('#config_form :input');
+						var id = null;
+						$form.each(function(){
+							if ( this.type != "button" ){
+								var val = ( this.type == 'checkbox' || this.type == 'radio' ) ? ( ( jQuery(this).is(':checked') ) ? jQuery(this).val() : null ) : jQuery(this).val();
+								if(val != null) {
+									dataString += "&"+this.name+"="+val;
+								}
+							}
+						});
+					 	
+						//make ajax call
+						jQuery.ajax({
+							type	:	"POST",
+							url		:	'start.php?task=finalise',
+							data	:	dataString,
+							dataType:	'json',
+							success	:	function(data)
+										{
+											window.location = 'index.php';
+										}
+						});
+
+					 }
 						jQuery( document ).ready( function ()
 						{
 							jQuery( '#go' ).click( function( ) {
 								jQuery( '.progress' ).removeClass( 'hide' );
 								jQuery( '.progressMsg' ).removeClass( 'hide' );
-								jQuery( '#go' ).addClass( 'hide' );
+								//jQuery( '#go' ).addClass( 'hide' );
 								setTimeout( function() { progress(); }, 1000 );
 								jQuery.ajax( { 'url': 'start.php', 'data': { 'task': 'go' }, 'type': 'post', 'dataType': 'text' } )
 										.done( function() {
 											jQuery( '.progress-bar' ).css( 'width',  '100%' );
 											jQuery( '.label-info' ).html(  '100%' );
 											finished = true;
-											window.location = 'index.php';
+											install();
 										} );
 							} );
 						} )
